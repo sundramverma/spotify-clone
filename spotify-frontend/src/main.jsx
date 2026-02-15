56cdeb303903c460f0728eea7daa78b3396f6e20
Sundram Verma <sundramverma0562@gmail.com>
2026-02-15 10:43:22 +0530

    Fix React Router setup for Vercel

--- spotify-frontend/src/main.jsx
+++ spotify-frontend/src/main.jsx
@@ -1,12 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { BrowserRouter } from 'react-router-dom'
-import PlayerContextProvider from './context/PlayerContext'
-import PlaylistContextProvider from './context/PlaylistContext'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import App from "./App.jsx";
+import "./index.css";
+
+import PlayerContextProvider from "./context/PlayerContext";
+import PlaylistContextProvider from "./context/PlaylistContext";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
       <PlayerContextProvider>
@@ -15,5 +17,5 @@ ReactDOM.createRoot(document.getElementById('root')).render(
         </PlaylistContextProvider>
       </PlayerContextProvider>
     </BrowserRouter>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
